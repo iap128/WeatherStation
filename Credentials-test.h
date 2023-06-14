8cbf9ca394fc8b6338fca317ff505900ba9acb41
iap128 <103337777+iap128@users.noreply.github.com>
2023-06-14 15:33:27 -0700

    Removed default creds file

--- Credentials-test.h
+++ Credentials-test.h
@@ -1,9 +1,14 @@
+//This is a test file with credentials that you can add
+//Remove the '-test' from the file name and fill in your info
+
 #ifndef Credentials_h
 #define Credentials_h
 
 const char * ssid = "";
 const char * password = "";
 
-char dbUser[] = "123";
+char dbUser[] = "";
 char dbPassword[] = "";
 IPAddress server(10, 0, 1, 1);
+
+#endif
